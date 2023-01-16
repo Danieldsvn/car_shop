07d1cfb775e4463533c696df4f7cc2471e5498db
Daniel Saraiva <danieldsvn@gmail.com>
2023-01-16 12:17:35 -0300

    Cria controller de Car

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import ICar from '../Interfaces/ICar';
+import CarService from '../Services/CarService';
+
+class CarController {
+  private req: Request;
+  private res: Response;
+  private service: CarService;
+
+  constructor(req: Request, res: Response) {
+    this.req = req;
+    this.res = res;
+    this.service = new CarService();
+  }
+
+  public async create() {
+    const car: ICar = {
+      model: this.req.body.model,
+      year: this.req.body.year,
+      color: this.req.body.color,
+      status: this.req.body.status,
+      buyValue: this.req.body.buyValue,
+      doorsQty: this.req.body.doorsQty,
+      seatsQty: this.req.body.seatsQty,
+    };
+
+    try {
+      const newCar = await this.service.create(car);
+      return this.res.status(201).json(newCar);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export default CarController;
